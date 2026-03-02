be937d754065f8319c9aa82047ed24e2806d1d15
vyommishra10-sketch <vyommishra.10@gmail.com>
2026-03-02 16:11:20 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,17 @@
 public class PalindromeCheckerApp {
     public static void main(String args[]) {
-
+        String word = "madam";
+        String reversed = "";
+        for(int i = word.length()-1;i >= 0; i--){
+            reversed = reversed + word.charAt(i);
+        }
+        System.out.println("Input text:" + word);
+        if (word.equals(reversed)) {
+            System.out.println("Is it a palindrome " + "True");
+        }
+            else
+        {
+            System.out.println("Is it a palindrome " + "False");
+        }
     }
 }
