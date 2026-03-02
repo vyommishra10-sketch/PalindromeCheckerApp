55e020b2eda8157daec8658ac50f550398131573
Vyom Misra <vyommishra.10@gmail.com>
2026-03-02 15:58:55 +0530

    Implement main method for Palindrome Checker App
    Added main method with welcome messages.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,7 @@
 public class PalindromeCheckerApp {
+   public static void main(String args[]) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
